e9be275b38c6e3d371306d5a50f2ff6c46c34a8e
Patrick Corcoran <patrickcorcoran10@gmail.com>
2026-08-07 15:50:54 -0500

    removed apostrofy from text

--- src/app/snippets/[id]/not-found.tsx
+++ src/app/snippets/[id]/not-found.tsx
@@ -1,5 +1,5 @@
 export default function SnippetNotFound() {
     return (
-        <div className="text-xl font-bold">sorry but we couldn't find that file</div>
+        <div className="text-xl font-bold">sorry but we could not find that file</div>
     )
 }
